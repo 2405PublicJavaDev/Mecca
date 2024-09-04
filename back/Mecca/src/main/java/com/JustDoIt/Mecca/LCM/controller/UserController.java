ead7f1389a8f40ceddd99a56c6cf7db8447593e8
2CM_Oracle <chaemin.office@gmail.com>
2024-09-04 18:59:51 +0900

    [Hotfix] api 사용을 위한 수정
    react에서 api 사용하기 위해서 vite config에 proxy로 api가 되어 있어서 Controller의 RequestMapping도 수정 해야 함

--- back/Mecca/src/main/java/com/JustDoIt/Mecca/LCM/controller/UserController.java
+++ back/Mecca/src/main/java/com/JustDoIt/Mecca/LCM/controller/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("/user")
+@RequestMapping("/api/user")
 public class UserController {
 
     private UserService service;
